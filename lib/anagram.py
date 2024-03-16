1d1f6ed87b735285424a9528102ac00afe9c1e0e
Dadir93 <abdifatah.farah@student.moringaschool.com>
2024-03-16 20:36:44 +0300

    done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,11 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, words_list):
+        anagrams = []
+        sorted_word = sorted(self.word.lower()) 
+        for word in words_list:
+            if word.lower() != self.word.lower() and sorted(word.lower()) == sorted_word:
+                anagrams.append(word)
+        return anagrams
